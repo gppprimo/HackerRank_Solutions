5d40fdf40aebfbff0fd472a3fb20a1b53ada66ac
Peppe <giupeppe0db@gmail.com>
2020-04-08 17:04:14 +0200

    Counter_Games_added

---
+++ Solutions/func.h
@@ -0,0 +1,30 @@
+//
+// Created by Peppe on 03/10/20.
+//
+
+#ifndef TEST_FUNC_H
+#define TEST_FUNC_H
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using std::string;
+using std::stringstream;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::unordered_map;
+using std::unordered_set;
+using std::pow;
+
+
+bool check_pow_two(long);
+long next_lower_pow_two(long);
+string counterGame(long n);
+
+#endif //TEST_FUNC_H
